23f5813a826069532ef523ddc0d83ef5f8e6216b
jrodri <jrodri@elpuig.xeill.net>
2021-10-07 17:57:32 +0200

    2.0

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -20,37 +20,86 @@ public class Game {
     int valorDau3;
 
     void juego(){
-        System.out.println("HOLA JUGADORES!");
-        System.out.print(menu.pedirJugador1);
-        jugador1.nombre = sc.nextLine();
-        System.out.println(menu.PedirJugador2);
-        jugador2.nombre = sc.nextLine();
-        boolean a = true;
-        int jug = 0;
-        while (a) {
-            jug ++;
+        while (true){
+            System.out.println(menu.menuInicio);
+            System.out.println(menu.menuJuegos);
 
-            System.out.println(menu.menuJuego1);
-            int opcion = sc.nextInt();
-            switch (opcion) {
-                case 1:
-                    jugador1.intentos++;
-                    valorDau1 = dau1.tirar();
-                    valorDau2 = dau2.tirar();
-                    valorDau3 = dau3.tirar();
-                    if (valorDau1 == valorDau2 && valorDau2 == valorDau3) {
-                        jugador1.puntos++;
+            int juego = sc.nextInt();
+            if (juego == 1){
+
+                System.out.println("HOLA JUGADORES!");
+
+                System.out.print(menu.pedirJugador1);
+                jugador1.nombre = sc.nextLine();
+
+                System.out.println(menu.PedirJugador2);
+                jugador2.nombre = sc.nextLine();
+
+                boolean a = true;
+                int jug = 0;
+
+                while (a) {
+                    jug ++;
+
+                    if (jug % 2 == 0){
+
+                        System.out.println(menu.menuJugador1);
+                        int opcion = sc.nextInt();
+                        switch (opcion) {
+                            case 1:
+                                jugador1.intentos++;
+                                valorDau1 = dau1.tirar();
+                                valorDau2 = dau2.tirar();
+                                valorDau3 = dau3.tirar();
+                                if (valorDau1 == valorDau2 && valorDau2 == valorDau3) {
+                                    jugador1.puntos++;
+                                }
+                                break;
+                            case 2:
+                                a = false;
+                        }
+
+                    }else{
+
+                        System.out.println(menu.menuJugador2);
+                        int opcion = sc.nextInt();
+                        switch (opcion) {
+                            case 1:
+                                jugador2.intentos++;
+                                valorDau1 = dau1.tirar();
+                                valorDau2 = dau2.tirar();
+                                valorDau3 = dau3.tirar();
+                                if (valorDau1 == valorDau2 && valorDau2 == valorDau3) {
+                                    jugador2.puntos++;
+                                }
+                                break;
+                            case 2:
+                                a = false;
+                        }
+
                     }
-                    break;
-                case 2:
-                    a = false;
+                }
+
+                System.out.println("JUGADOR 1\nNombre:   " + jugador1.nombre + "\nIntentos: " + jugador1.intentos + "\nPuntos:   " + jugador1.puntos);
+                System.out.println("JUGADOR 2\nNombre:   " + jugador2.nombre + "\nIntentos: " + jugador2.intentos + "\nPuntos:   " + jugador2.puntos);
+
+                if (jugador1.puntos > jugador2.puntos){
+                    System.out.println(menu.ganaJugador1);
+                }else if (jugador2.puntos > jugador1.puntos){
+                    System.out.println(menu.ganaJugador2);
+                }else{
+                    System.out.println(menu.empatan);
+                }
+
+            }else if (juego == 2){
+                System.out.println("NO HAY JUEGO");
+            }else if (juego == 3){
+                System.out.println("NO HAY JUEGO");
+            } else{
+                break;
             }
 
-
-
-
         }
-        System.out.println("JUGADOR 1\nNombre:   " + jugador1.nombre + "\nIntentos: " + jugador1.intentos + "\nPuntos:   " + jugador1.puntos);
     }
 
 }
