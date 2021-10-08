72c6b07868ce3eeeeb8225c6990428e5847cff9e
jrodri <jrodri@elpuig.xeill.net>
2021-10-08 02:07:33 +0200

    3.0

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -9,22 +9,21 @@ public class Menu {
         this.jugador2 = jugador2;
     }
 
-
     String menuInicio = "███    ███ ███████ ███    ██ ██    ██ \n" +
                         "████  ████ ██      ████   ██ ██    ██ \n" +
                         "██ ████ ██ █████   ██ ██  ██ ██    ██ \n" +
                         "██  ██  ██ ██      ██  ██ ██ ██    ██ \n" +
-                        "██      ██ ███████ ██   ████  ██████ \n ";
+                        "██      ██ ███████ ██   ████  ██████ \n";
 
     String menuJuegos = "1. DADOS\n" + "2. 3 EN RAYA\n" + "3. PARXIS\n" + "4. SALIR";
 
 
     String pedirJugador1 = "Hola jugador 1, escribe tu nombre: ";
     String PedirJugador2 = "Hola jugador 2, escribe tu nombre: ";
-    String menuJugador1 = "--TE TOCA--" + "jugador1.nombre" + "-----\n1 -> Tirar dados.\n2 -> Salir";
-    String menuJugador2 = "--TE TOCA--" + "jugador2.nombre" + "-----\n1 -> Tirar dados.\n2 -> Salir";
+    String menuJugador1 = "--TE TOCA--" + jugador1.nombre + "-----\n1 -> Tirar dados.\n2 -> Salir";
+    String menuJugador2 = "--TE TOCA--" + jugador2.nombre + "-----\n1 -> Tirar dados.\n2 -> Salir";
 
-    String ganaJugador1 = "jugador1.nombre" + " GANA!";
-    String ganaJugador2 = "jugador2.nombre" + " GANA!";
+    String ganaJugador1 = jugador1.nombre + " GANA!";
+    String ganaJugador2 = jugador2.nombre + " GANA!";
     String empatan = "EMPATE!";
 }
